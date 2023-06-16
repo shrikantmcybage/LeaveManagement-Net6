a15fdee63cb78fddc21a1bd59e3699be37f3dafa
Shrikant Mule <shrikantm@cybage.com>
2023-06-16 15:35:33 +0530

    Added database tables and migrations

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    { 
+        public int NumberOfDays { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; } 
+        public int LeaveTypeId { get; set; }
+
+        public String EmployeeId { get; set; }
+
+    }
+}
